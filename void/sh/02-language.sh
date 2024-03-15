c142dfec329584ea9581fe1a0d656372a66b139f
awbtux <andy@antareslinux.org>
2024-03-15 01:25:40 -0700

    sed language

--- void/sh/02-language.sh
+++ void/sh/02-language.sh
@@ -2,3 +2,6 @@
 
 # set the language
 printf "LANG=$language.UTF-8\nLC_ALL=$language.UTF-8\nLC_COLLATE=C" >$VDIR/etc/locale.conf
+
+# generate it
+sed "s/#${language}/${language}/g" -i $VDIR/etc/default/libc-locales
